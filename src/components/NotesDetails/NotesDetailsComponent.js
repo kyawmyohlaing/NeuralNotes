cb231dd1b7c418479a8accda12316739cb7954df
DarthVanger <darthvanger@gmail.com>
2020-06-11 21:02:02 +0300

    Copy note name text editor

--- src/components/NotesDetails/NotesDetailsComponent.js
+++ src/components/NotesDetails/NotesDetailsComponent.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { StyledNoteEditorTextArea } from 'components/NoteNameEditor/NoteNameEditorStyles';
 
 export class NotesDetailsComponent extends Component {
   render() {
-    return <p>Hello world!</p>;
+    return (
+      <p
+        style={{
+          background: 'white',
+          width: '200px',
+          height: '200px',
+          position: 'absolute',
+          zIndex: '10000',
+          top: '0',
+          left: '0',
+        }}>
+        <StyledNoteEditorTextArea onChange={() => {}} value={'asdf'} />
+      </p>
+    );
   }
 }
